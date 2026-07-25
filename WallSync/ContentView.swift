182f463ec58ec5c6c96e54479e4d7c5eefc71519
WenfengYe <wenfeng110402@icloud.com>
2026-07-25 21:58:01 +0800

    Initial Commit

---
+++ WallSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WallSync
+//
+//  Created by 叶文峰 on 2026/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WallSync/WallSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallSyncApp.swift
+//  WallSync
+//
+//  Created by 叶文峰 on 2026/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WallSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
